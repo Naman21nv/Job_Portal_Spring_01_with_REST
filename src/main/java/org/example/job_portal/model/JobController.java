692d37ff08dcb33759b7701845a4c39c943d6565
Naman21nv <namannv28@gmail.com>
2026-03-18 07:25:25 +0530

    JobController got added

---
+++ src/main/java/org/example/job_portal/model/JobController.java
@@ -0,0 +1,26 @@
+package org.example.job_portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+
+
+    @RequestMapping({"/","home"})
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("/addjob")
+    public String addJob(){
+        return "addjob";
+    }
+
+
+    @RequestMapping("handleForm")
+    public String handleForm(){
+        return "sucess";
+    }
+
+}
